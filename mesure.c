061fc54c161c3ede70a00ed1118305122a57dfee
3846504 <38dori.504@ezweb.ne.jp>
2021-09-01 17:23:17 +0900

    割り込みを禁止

--- mesure.c
+++ mesure.c
@@ -230,7 +230,7 @@ void *get_data(void *arg){
 
             for(int n=i/buf_num%width; n<i/buf_num%width+1; n++){
                 for(int m=0; m<height; m++){
-                    fbptr[m*width+n] = BLACK;
+                    fbptr[m*width+n] = WHITE;
                 }
             }
 
@@ -315,8 +315,120 @@ void *check_button(void *arg){
     }
 }
 
+void graph(){
+    int miso = MISO;
+    int mosi = MOSI;
+    int clk = CLK;
+    int cs = CS;
+
+    int white = WHITE;
+    int gray = GRAY;
+    int blue = BLUE;
+    int yellow = YELLOW;
+
+    int width = WIDTH;
+    int height = HEIGHT;
+
+    int fb = open(DEV_FB, O_RDWR | O_SYNC);
+    if(fb < 0) {
+        fprintf(stderr, "framebuffer open error\n");
+    }
+
+    fbptr = (int *)mmap(0, width*height*4, 
+                        PROT_READ | PROT_WRITE, MAP_SHARED,
+                        fb, 0);
+
+    gpio_init();
+
+    gpio_configure(miso, GPIO_INPUT);
+    gpio_configure(mosi, GPIO_OUTPUT);
+    gpio_configure(clk, GPIO_OUTPUT);
+    gpio_configure(cs, GPIO_OUTPUT);
+
+    int time = 0;
+    int count = 0;
+
+    int value;
+    int v0 = 0;
+    int v1 = 0;
+
+    int i = 0;
+
+    for(;;){
+        if(flag == 1){
+            printf("stop mesuring\n");
+            break;
+        }
+
+        value = spi_xfer(miso, mosi, clk, cs, 0x600000);
+
+        if(i%buf_num == 0){
+            v1 = v0;
+            v0 = value;
+
+            for(int n=i/buf_num%width; n<i/buf_num%width+1; n++){
+                for(int m=0; m<height; m++){
+                    fbptr[m*width+n] = BLACK;
+                }
+            }
+
+            drawLine(i/buf_num%width, height-120-v0/8, i/buf_num%width+1, height-120-v1/8, 0xFF0000, fbptr);
+        }
+
+        if(flag == 2){
+            save_data[count] = v0;
+            count++;
+            if(count == save_num-1){
+                printf("finish mesuring\n");
+                flag = 0;
+                count = 0;
+            }
+        }
+        i++;
+    }
+
+    close(fb);
+}
+
 int main()
 {
+    int mem_fd;
+    char *map;
+    int i;
+    int irq1, irq2, irq3;
+
+    volatile unsigned int *irqen1;
+    volatile unsigned int *irqen2;
+    volatile unsigned int *irqen3;
+    volatile unsigned int *irqdi1;
+    volatile unsigned int *irqdi2;
+    volatile unsigned int *irqdi3;
+
+    if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0) return -1;
+    map = (char*) mmap(NULL,
+            4096,
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED,
+            mem_fd,
+            PERI_ADD + 0xb000
+            );
+    
+    if (map == MAP_FAILED)  return -1;
+    irqen1 = (volatile unsigned int *) (map + 0x210);
+    irqen2 = (volatile unsigned int *) (map + 0x214);
+    irqen3 = (volatile unsigned int *) (map + 0x218);
+    irqdi1 = (volatile unsigned int *) (map + 0x21c);
+    irqdi2 = (volatile unsigned int *) (map + 0x220);
+    irqdi3 = (volatile unsigned int *) (map + 0x224);
+
+    irq1 = *irqen1;
+    irq2 = *irqen2;
+    irq3 = *irqen3;
+    // 以下3行で全部の割り込みを禁止する
+    *irqdi1 = 0xffffffff;
+    *irqdi2 = 0xffffffff;
+    *irqdi3 = 0xffffffff;
+
     pthread_t check_thread;
     pthread_t get_data_thread;
     pthread_t check_button_thread;
@@ -337,6 +449,12 @@ int main()
     for(int i=0; i<save_num; i++){
         fprintf(file, "%d\n", save_data[i]);
     }
+
+    fclose(file);
+
+    *irqen1 = irq1;
+    *irqen2 = irq2;
+    *irqen3 = irq3;
 
     return 0;
 }
