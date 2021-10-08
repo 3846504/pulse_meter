9a966c9d3dad7390195b26002b0a06a8b4df0596
3846504 <38dori.504@ezweb.ne.jp>
2021-10-08 16:06:15 +0900

    shellスクリプトファイルの変更

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 sudo cp /opt/sakulab_ex_module/boot/config.txt /boot/config.txt
 
 #スリープの無効化
-@xset s off >> /etc/xdg/lxsession/LXDE/autostart
-@xset s noblank >> /etc/xdg/lxsession/LXDE/autostart
-@xset -dpms >> /etc/xdg/lxsession/LXDE/autostart
+echo @xset s off >> /etc/xdg/lxsession/LXDE/autostart
+echo @xset s noblank >> /etc/xdg/lxsession/LXDE/autostart
+echo @xset -dpms >> /etc/xdg/lxsession/LXDE/autostart
 
-[SeatDefaults] >> /etc/lightdm/lightdm.conf
-xserver-command=X -s 0 -dpms >> /etc/lightdm/lightdm.conf
+echo [SeatDefaults] >> /etc/lightdm/lightdm.conf
+echo xserver-command=X -s 0 -dpms >> /etc/lightdm/lightdm.conf
 
 #権限の変更
 sudo chmod 755 /opt/sakulab_ex_module/src/start_program.sh
@@ -18,6 +18,10 @@ sudo chmod 755 /opt/sakulab_ex_module/bin/tp_mm
 #serviceファイルの有効化
 sudo cp /opt/sakulab_ex_module/src/StartMesure.service /etc/systemd/system
 sudo systemctl enable StartMesure
+
+#必要コマンドの入手
+sudo apt-get update
+sudo apt-get install fbi
 
 #再起動
 sudo reboot
