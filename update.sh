9129a3a17c949b87e6a09b83e443204fc05e70f1
3846504 <38dori.504@ezweb.ne.jp>
2021-11-18 01:45:31 +0900

    update.shを変更

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #githubからの取得(強制にリモートに合わせる)
-git fetch origin master
-git reset --hard origin/master
+sudo git -C /opt/sakulab_ex_module fetch origin master
+sudo git -C /opt/sakulab_ex_module reset --hard origin/master
 
 #権限の変更
 sudo chmod 755 /opt/sakulab_ex_module/src/start_program.sh
