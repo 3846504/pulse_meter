da1f85c05b28edf0282d4388327a4d520caf9db3
3846504 <38dori.504@ezweb.ne.jp>
2021-11-18 00:42:45 +0900

    update用スクリプトを作成

---
+++ update.sh
@@ -0,0 +1,9 @@
+#githubからの取得
+git -C /opt/sakulab_ex_module pull
+
+#権限の変更
+sudo chmod 755 /opt/sakulab_ex_module/src/start_program.sh
+sudo chmod 755 /opt/sakulab_ex_module/bin/chua_mm
+sudo chmod 755 /opt/sakulab_ex_module/bin/pulse_meter
+sudo chmod 755 /opt/sakulab_ex_module/bin/tp_mm
+sudo chmod 755 /opt/sakulab_ex_module/bin/amp_mm
