052d3c515e2a60f3604a87107f41482682e97595
3846504 <38dori.504@ezweb.ne.jp>
2021-11-18 00:49:18 +0900

    update.shの修正

--- update.sh
+++ update.sh
@@ -8,3 +8,8 @@ sudo chmod 755 /opt/sakulab_ex_module/bin/chua_mm
 sudo chmod 755 /opt/sakulab_ex_module/bin/pulse_meter
 sudo chmod 755 /opt/sakulab_ex_module/bin/tp_mm
 sudo chmod 755 /opt/sakulab_ex_module/bin/amp_mm
+
+#serviceファイルの有効化
+sudo cp /opt/sakulab_ex_module/src/StartMesure.service /etc/systemd/system
+sudo systemctl enable StartMesure
+sudo systemctl start StartMesure
